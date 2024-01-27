bec4bae52d9a82e7a81d351f7986a07b61f0aedb
bsdaoquang <bsdaoquang@gmail.com>
2024-01-27 20:38:49 +0700

    add sesson-4

--- sesson-3/src/App.jsx
+++ sesson-3/src/App.jsx
@@ -1,8 +1,6 @@
 /** @format */
 
 import React from 'react';
-import IncrementScreen from './screens/IncrementScreen';
-import Products from './screens/Products';
 import ProfileScreen from './screens/ProfileScreen';
 
 const App = () => {
